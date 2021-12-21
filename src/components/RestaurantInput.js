c1df45941cc16a50da9fc2c4b5cb999ffb66aadf
Patrick Brennan <pbrennanmusic@gmail.com>
2021-12-21 16:20:19 -0500

    commit tree af6143b8857ef5c0c3f7d4ee86e5759b7d657d6b

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -49,10 +49,6 @@ export class RestaurantInput extends Component {
   }
 };
 
-const mapStateToProps = state => {
-  return state;
-}
-
 const mapDispatchToProps = dispatch => {
   return {
     addRestaurant: (restaurant) => {
@@ -61,4 +57,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(RestaurantInput)
+export default connect(null, mapDispatchToProps)(RestaurantInput)
